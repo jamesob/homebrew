344af191f31027ff362e4c923e1e987e30ae62c2
James O'Beirne <james.obeirne@gmail.com>
2015-11-19 22:04:32 -0800

    Bump desk to 0.3.1

--- Library/Formula/desk.rb
+++ Library/Formula/desk.rb
@@ -1,7 +1,7 @@
 class Desk < Formula
   desc "Lightweight workspace manager for the shell"
   homepage "https://github.com/jamesob/desk"
-  url "https://github.com/jamesob/desk/archive/v0.1.2.tar.gz"
+  url "https://github.com/jamesob/desk/archive/v0.3.1.tar.gz"
   sha256 "db6dfb80cbb012adf925a8002ce43e5e021b947917de4992bad06fc3cab16d23"
 
   def install
